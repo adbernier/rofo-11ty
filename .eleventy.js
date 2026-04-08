1e3e7c5071b391ea527d0af2f74e51e5be12a0f5
Alan Bernier <alanbernier@Alans-MacBook-Air-2.local>
2026-04-08 11:28:30 -0700

    Prepare Rofo static site for deploy

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,8 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("styles.css");
   eleventyConfig.addPassthroughCopy("js");
-
-  return {
-    dir: {
-      input: ".",
-      includes: "_includes",
-      output: "_site"
-    }
-  };
-};
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("styles.css");
-  eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("assets"); // ← ADD THIS
 
   return {
     dir: {
