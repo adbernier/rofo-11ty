164e502a9dd076efab555ae84fa9cd361cd95ecb
Alan Bernier <alanbernier@Alans-MacBook-Air-2.local>
2026-04-19 10:28:16 -0700

    Add favicon set + base template links (site-wide)

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,10 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("favicon.ico");
+  eleventyConfig.addPassthroughCopy("favicon-32x32.png");
+  eleventyConfig.addPassthroughCopy("favicon-16x16.png");
+  eleventyConfig.addPassthroughCopy("apple-touch-icon.png");
 
   eleventyConfig.addFilter("uniqueStates", (cities) => {
     const seen = new Set();
