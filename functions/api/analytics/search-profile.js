0f65b026cee830070f4509eca6c04331e800bf96
Alan Bernier <alan@rofo.com>
2026-07-08 13:17:08 -0700

    feat: add example location brief page

--- functions/api/analytics/search-profile.js
+++ functions/api/analytics/search-profile.js
@@ -20,6 +20,8 @@ const ALLOWED_EVENTS = new Set([
   "find_locations_primary_cta_clicked",
   "recommendation_prompt_viewed",
   "recommendation_prompt_clicked",
+  "example_location_brief_viewed",
+  "example_location_brief_cta_clicked",
 ]);
 
 const BOT_USER_AGENT_PATTERN = /googlebot|bingbot|ahrefs|semrush|dotbot|mj12bot|petalbot|facebookexternalhit|twitterbot|slackbot|linkedinbot|yandex|baiduspider|duckduckbot|applebot|gptbot|chatgpt-user|ccbot|bot\b|crawler|spider/i;
